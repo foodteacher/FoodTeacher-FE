2fe1eb1256090822209649cd7a1c098e40124c87
최현우 <hw.choi@okestro.com>
2023-12-26 18:45:36 +0900

    design(header) : header 분기 처리

--- component/header/TheMainHeader.tsx
+++ component/header/TheMainHeader.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { usePathname, useRouter } from "next/navigation";
 import TextLogoIcon from "../icon/TextLogoIcon";
 import UserIcon from "../icon/UserIcon";
+import GoBackArrowIcon from "../icon/GoBackArrowIcon";
 
 const TheMainHeader = () => {
   const path = usePathname();
@@ -11,24 +12,46 @@ const TheMainHeader = () => {
 
   return (
     <>
-      {path === "/main" && (
-        <Flex
-          as={"header"}
-          position={"absolute"}
-          top={"0"}
-          padding={"4px 22px"}
-          w={"100%"}
-          h={"52px"}
-          alignItems={"center"}
-          justifyContent={"space-between"}
-          zIndex={1000}
-        >
-          <TextLogoIcon />
-          <Text onClick={() => router.push("/mypage")} cursor={"pointer"}>
-            <UserIcon />
-          </Text>
-        </Flex>
-      )}
+      <Flex
+        as={"header"}
+        position={"absolute"}
+        top={"0"}
+        padding={"4px 22px"}
+        w={"100%"}
+        h={"52px"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        zIndex={1000}
+      >
+        {path === "/main" && (
+          <>
+            <TextLogoIcon />
+            <Text onClick={() => router.push("/mypage")} cursor={"pointer"}>
+              <UserIcon />
+            </Text>
+          </>
+        )}
+
+        {path === "/detail" && (
+          <>
+            <Box
+              pos={"absolute"}
+              cursor={"pointer"}
+              onClick={() => router.push("/main")}
+            >
+              <GoBackArrowIcon />
+            </Box>
+            <Text
+              margin={"0 auto"}
+              color={"#FFFFFF"}
+              fontSize={"20px"}
+              fontWeight={"medium"}
+            >
+              레포트 상세보기
+            </Text>
+          </>
+        )}
+      </Flex>
     </>
   );
 };
