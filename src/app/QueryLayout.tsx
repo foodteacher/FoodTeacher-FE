7aac691bc543174021ad7cf627b9fd449f1b2878
chhw130 <527coco@naver.com>
2024-01-05 12:36:16 +0900

    feat : useUser
    user정보 가져오는 hook구성

--- src/app/QueryLayout.tsx
+++ src/app/QueryLayout.tsx
@@ -2,9 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { redirect, useRouter } from "next/navigation";
 import { PropsWithChildren, useEffect, useState } from "react";
-import { useUser } from "../../utils/hooks/useUser";
 
 export default function QueryLayout({ children }: PropsWithChildren) {
   const [queryClient] = useState(
@@ -12,12 +10,10 @@ export default function QueryLayout({ children }: PropsWithChildren) {
       new QueryClient({
         defaultOptions: {
           queries: {
-            // onError: queryErrorHandler,
             refetchOnWindowFocus: false,
             refetchOnReconnect: false,
           },
           mutations: {
-            // onError: queryErrorHandler,
             retry: false,
           },
         },
@@ -33,7 +29,7 @@ export default function QueryLayout({ children }: PropsWithChildren) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {/* {product && <ReactQueryDevtools initialIsOpen={false} />} */}
+      {<ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   );
 }
