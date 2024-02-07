48e8190047d3293459ef116e7362b6b41aae70cf
chhw130 <527coco@naver.com>
2024-02-07 22:21:28 +0900

    feat : 칼로리계산 챗봇 위젯
    Refs:[FD-2]

--- component/chatbot/widget/MenuWidget.tsx
+++ component/chatbot/widget/MenuWidget.tsx
@@ -4,19 +4,19 @@ import React from "react";
 import { Button, Card, CardBody, VStack } from "@chakra-ui/react";
 
 const MenuWidget = (props: any) => {
-  const selectCaloryCal = () => {
-    props.actions.initialAction();
+  const selectCaloryCalculate = () => {
+    props.actions.caloryCalAction();
   };
 
   const selectTodayCal = () => {
-    props.actions.afterNameMessage();
+    props.actions.calculateNutrient();
   };
 
   return (
     <Card>
       <CardBody>
         <VStack>
-          <Button onClick={selectCaloryCal} w={"100%"}>
+          <Button onClick={selectCaloryCalculate} w={"100%"}>
             칼로리 계산
           </Button>
           <Button w={"100%"} onClick={selectTodayCal}>
--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -11,6 +11,7 @@ const page = () => {
   return (
     <>
       <Chatbot
+        // @ts-ignore
         config={config}
         messageParser={MessageParser}
         actionProvider={ActionProvider}
